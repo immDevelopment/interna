7704dc17c661509226dfe2af279289c0216be1e4
doriandesings <doriandesings@hotmail.com>
2018-02-28 13:50:58 +0100

    Gráfico con los porcentajes. Faltan los dias

---
+++ dev/js/days.js
@@ -0,0 +1,7 @@
+let today = new Date("01/01/1998");
+
+console.log(today.getDate()-14);
+
+const getDays = (element) =>{
+
+}
--- dev/js/planificacion.js
+++ dev/js/planificacion.js
@@ -8,10 +8,11 @@ const dataGet = (get) => {
         if (xhr.readyState === 4 && xhr.status === 200) {
             respuesta(xhr);
         }
-    }
+    };
+
     xhr.open('GET', 'php/planificacion.php?tsql=' + get, true);
     xhr.send();
-}
+};
 
 // Se imprimen los datos
 const respuesta = (xhr) => {
@@ -20,8 +21,10 @@ const respuesta = (xhr) => {
 
     let dataElement = document.getElementById('data');
 
+    /*Funcion en viewport.js para saber si es movil/tablet o desktop*/
     let breakPoint = matchMedia('(min-width: 768px)');
 
+    /*Si es movil...*/
     if (!changeSize(breakPoint)) {
 
         for (let data of json.data) {
@@ -46,6 +49,7 @@ const respuesta = (xhr) => {
         items = [...document.querySelectorAll('.data__info')];
         icons = [...document.querySelectorAll('.data__icon')];
 
+    /*Si es desktop*/
     } else {
 
         dataElement.innerHTML += `
@@ -55,7 +59,7 @@ const respuesta = (xhr) => {
                     <div class="dataTable__text">Proyecto</div>
                     <div class="dataTable__text">Fecha Aprobación</div>
                     <div class="dataTable__text">Fecha Fin</div>
-                    <div class="dataTable__graphic">Dias del mes</div>
+                    <div class="dataTable__graphic">Poner Dias</div>
                 </div>
         `;
 
@@ -80,43 +84,50 @@ const respuesta = (xhr) => {
         if (id = 1) {
             let sql = proyectoSQL;
         }
-    }
+    };
 
     /*Array para objetos con los id y las duraciones*/
     let durationsObj = [];
 
+    /*Array de duraciones para calcular las más larga*/
     let allDurations=[];
 
-    /*Copio en durations todas las duraciones*/
+    /*Copio en el array los id y duraciones de cada objeto*/
     for(let i=0;i<json.data.length;i++){
+
         durationsObj[i] = {
             id: json.data[i].CodigoProyecto,
             duration: json.data[i].duracion
         };
 
+        /*Añado duraciones al array*/
         allDurations.push(json.data[i].duracion);
     }
 
+    /*Calculo la máxima duración*/
     let maxDuration = Math.max(...allDurations);
 
     console.log(maxDuration);
 
+    /*Función para calcular el ancho del gráfico*/
     const graphicWidth = (id, duration) =>{
 
         let graphic = document.getElementById(id);
 
         /*Calculo de la anchura*/
-
         let width = (duration * 100) / maxDuration;
 
         graphic.style.width = `${width}%`;
-        graphic.textContent = `${(width.toFixed(2).endsWith('00')) ? width.toFixed(0):width.toFixed(2)}%`;
-    }
 
+        /*Porcentaje en el gráfico si tiene 00 no salen*/
+        graphic.textContent = `${(width.toFixed(2).endsWith('00')) ? width.toFixed(0):width.toFixed(2)}%`;
+    };
+
+    /*Por cada objeto calculo el ancho para dibujarlo*/
     for (let data of durationsObj){
         graphicWidth(data.id, data.duration);
     }
-}
+};
 
 
 dataGet("proyecto");
